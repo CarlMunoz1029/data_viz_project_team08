085c0543a9fb57f14deb5551a4d1a557c75aedde
unknown <guayhottin.rose@hotmail.com>
2023-06-19 22:03:28 -0400

    updated preprocess

--- preprocess.py
+++ preprocess.py
@@ -16,14 +16,14 @@ def preprocess_general_timeline(df_tl):
     # convert to datetime format
     df_tl['DAY'] = pd.to_datetime(df_tl['DAY'])
 
-    df_tl['END_RANGE_HOSP'] = df_tl['DAY'].copy()
-    df_tl['END_RANGE_FALL'] = df_tl['DAY'].copy()
-    df_tl['END_RANGE_PAIN'] = df_tl['DAY'].copy()
-
     # change True/False to int, with real data would maybe have count
     df_tl['HAS_PAIN_MENTION'] = df_tl['HAS_PAIN_MENTION'].astype(int)
 
     # add column for timeline x_end
+    df_tl['END_RANGE_HOSP'] = df_tl['DAY'].copy()
+    df_tl['END_RANGE_FALL'] = df_tl['DAY'].copy()
+    df_tl['END_RANGE_PAIN'] = df_tl['DAY'].copy()
+    
     df_tl.loc[list(df_tl.loc[df_tl['HOSPITALIZATION_COUNT']!=0].index.values),'END_RANGE_HOSP'] += timedelta(days=1)
     df_tl.loc[list(df_tl.loc[df_tl['FALL_COUNT']!=0].index.values),'END_RANGE_FALL'] += timedelta(days=1)
     df_tl.loc[list(df_tl.loc[df_tl['HAS_PAIN_MENTION']!=0].index.values),'END_RANGE_PAIN'] += timedelta(days=1)
